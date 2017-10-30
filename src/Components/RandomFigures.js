1abc018d06cfd505a4d4e72210902540f99c842b
ghevond20 <ghevond20@gmail.com>
2017-10-31 00:52:16 +0400

    Test Addd

---
+++ src/Components/RandomFigures.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Figure from './Figures'
+
+export default class RandomFigures extends Component {
+  render() {
+    return (<div >
+    <Figure/>
+    <Figure/>
+    <Figure/>
+    </div>
+    );
+  }
+}
---
+++ src/lib/Constants.js
@@ -0,0 +1,2 @@
+export const BOARD_ROW_COUNT =10
+export const BOARD_COL_COUNT = 10
